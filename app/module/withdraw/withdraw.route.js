05f6c76980ad88046a38cdcba28ff9f42d46811d
herdin lucky <38779904+hla12@users.noreply.github.com>
2025-11-29 01:52:46 +0700

    add middleware

--- app/module/withdraw/withdraw.route.js
+++ app/module/withdraw/withdraw.route.js
@@ -1,5 +1,13 @@
 const controller = require("./withdraw.controller");
 
+const accessKeyMiddleware = (req, res, next) => {
+  const accessKey = req.headers['Access-Key'];
+  if (accessKey !== 'memeksapikecap') {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+};
+
 module.exports = (app) => {
-  app.post("/withdraw/create", controller.handleWithdraw);
+  app.post("/withdraw/create", accessKeyMiddleware, controller.handleWithdraw);
 };
